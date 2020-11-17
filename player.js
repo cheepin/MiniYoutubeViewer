3d78ad6a9cac230cc1140afb2e533854d00ec6b0
cheepin <moorhsum.aviv@gmail.com>
2020-11-18 04:18:29 +0900

    add css sheet for stack button adove player

--- player.js
+++ player.js
@@ -1,5 +1,5 @@
 const {ipcRenderer} = require('electron')
-const asyncMsgBtn = document.getElementById("read-button");
+const readBtn = document.getElementById("read-button");
 const closeBtn = document.getElementById("close-button");
 const size_offset_width = 0.95;
 const size_offset_height = 0.92;
@@ -41,9 +41,7 @@ ipcRenderer.on("resized-window", (event, _width, _height) => {
   player.setSize(_width * size_offset_width, _height * size_offset_height);
 })
 
-// The API calls this function when the player's state changes.
-// The function indicates that when playing a video (state=1),
-// the player should play for six seconds and then stop.
+// on Youtube StateChange
 var done = false;
 function onPlayerStateChange(event) {
   // if (event.data == YT.PlayerState.PLAYING && !done) {
@@ -52,15 +50,14 @@ function onPlayerStateChange(event) {
   // }
 }
 
-// on Async Message Button Clicked
-console.log(asyncMsgBtn);
-asyncMsgBtn.addEventListener('click', () => {
+// on Read Button Clicked
+readBtn.addEventListener('click', () => {
   tag.src = "https://www.youtube.com/iframe_api";
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  // ipcRenderer.invoke('asynchronous-message', 'ping')
 })
 
+// on Close Button Clicked
 closeBtn.addEventListener('click', () => {
   ipcRenderer.invoke('close-button-pressed');
 })
