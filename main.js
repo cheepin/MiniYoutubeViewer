7274b0dddc914b18f6bdf04623fef12f1e13eb4a
cheepin <moorhsum.aviv@gmail.com>
2020-11-22 15:15:52 +0900

    add custom schema setting function

--- main.js
+++ main.js
@@ -2,36 +2,78 @@ const {
   app,
   BrowserWindow,
   ipcMain,
-  ipcRenderer,
+  remote,
   dialog,
 } = require('electron')
 
-// on App Start
 const debug_path = 'youtube://7TA_VPCh7K0/?width=1920&height=1080'
-const whole_args = process.argv[2]
-
-try{
-  // receive arg
-  const args = whole_args.split('://')[1]
-  const splitted_params = args.split('?')
-  query =  splitted_params[1].split('&')
-
-    // parse arg
-  var param_keys = {}
-  param_keys['youtube_id'] = splitted_params[0]
-  query.forEach(item => {
-    key_value = item.split('=')
-    param_keys[key_value[0]] = key_value[1]
-    console.log(item)
-  });
+var whole_args = process.argv[1]
+if (process.env.NODE_ENV === "debug" || process.env.NODE_ENV === "release") {
+  whole_args = process.argv[2]
 }
-catch(e){
-  dialog.showErrorBox('args invalid', `non valid url or args: ${whole_args}`)
-  app.exit()
-}
+
+const custom_schema = 'miniyp'
+const NON_ARGS_LENGTH = 1
+
+// on App Start
+app.whenReady().then(()=>{
+  // Assume Initial App Boot
+  if (process.argv.length <= NON_ARGS_LENGTH){
+    var notice_win = new BrowserWindow({
+      width: 1920,
+      height: 1080,
+      resizable: true,
+      thickFrame: true,
+      autoHideMenuBar: true,
+      backgroundColor: "#FFFFFF",
+      webPreferences: {
+        nodeIntegration: true
+      },
+      alwaysOnTop: true,
+    });
+  
+    var options = {
+      type: 'info',
+      buttons: ['Yes', 'No'],
+      title: 'confirm',
+      message: 'confirm',
+      detail: `you must register the default protocol \'miniyp\' to use this app. \notherwise exit \n${process.argv}`
+    };
+    if (!dialog.showMessageBoxSync(notice_win, options)){
+      app.setAsDefaultProtocolClient(custom_schema, undefined, ['--', '%1'])
+    }
+    app.exit()
+  }
+
+  // Normal App Start
+  else {
+    try{
+      // receive arg
+      const args = whole_args.split('://')[1]
+      const splitted_params = args.split('?')
+      query =  splitted_params[1].split('&')
+    
+        // parse arg
+      var param_keys = {}
+      param_keys['youtube_id'] = splitted_params[0]
+      query.forEach(item => {
+        key_value = item.split('=')
+        param_keys[key_value[0]] = key_value[1]
+        console.log(item)
+      });
+    }
+    catch(e){
+      dialog.showErrorBox('args invalid', `non valid url or args: ${ process.argv}`)
+      app.exit()
+    }
+    createWindow(param_keys);
+
+  }
+})
+
 
 // on create window
-function createWindow () {
+function createWindow (param_keys) {
   const win = new BrowserWindow({
     width: parseInt(param_keys['width']),
     height: parseInt(param_keys['height']),
@@ -65,9 +107,6 @@ function createWindow () {
     win.webContents.send('resized-window', bounds.width, bounds.height);
   });
 }
-
-app.whenReady().then(createWindow)
-
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
